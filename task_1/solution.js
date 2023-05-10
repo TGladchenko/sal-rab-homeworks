33c82dd642e5b63aee27c1389f606d604020b565
Tatyana <tngld28@mail.ru>
2023-05-10 17:00:17 +0300

    Задание выполнено

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,52 +1,47 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
-
+  
     // Задание №1.1. Оповещение о добавлении в корзину
-
-    let message;// создайте переменную message
-    message = productName;// присвойте переменной message значение переменной productName
-    message = message + " ";// добавьте в конец переменной message пробел
-    message = message + "за";// добавьте в конец переменной message текст 'за'
-    message = message + " ";// добавьте в конец переменной message пробел
-    message = message + productPrice;// добавьте в конец переменной message значение переменной productPrice
-    message = message + " ";// добавьте в конец переменной message пробел
-    message = message + "теперь в корзине!";// добавьте в конец переменной message текст 'теперь в корзине!'
-    
-    console.log (message);// выведите переменную message в консоль
-
+  
+    // создайте переменную message
+    let message = productName + " за " + productPrice + " теперь в корзине!";
+  
+    // выведите переменную message в консоль
+    console.log(message);
+  
     // Конец решения задания №1.1.
-
+  
     return message;
-}
-
-function addInCartCountChange(value) {
-
+  }
+  
+  function addInCartCountChange(value) {
     // Задание №1.2. Увеличение количества в корзине
+  
     let oldValue = value;
-
-    let newValue;// создайте переменную newValue
-    newValue=oldValue;// присвойте ей значение oldValue
-    newValue=newValue + 1;// увеличьте значение newValue на 1
-
+  
+    // создайте переменную newValue
+    let newValue = oldValue + 1;
+  
     // Конец решения задания №1.2.
-
+  
     return newValue;
-}
-
-function addInCartSumChange(sum, delta) {
+  }
+  
+  function addInCartSumChange(sum, delta) {
     let oldSum = sum;
     let difference = delta;
-
+  
     // Задание №1.3. Увеличение суммы в корзине
-
-    let newSum;
-    newValue = oldValue + 1;// создайте переменную newSum и присвойте ей сумму oldSum и difference
-    let newSumText = newSum + ' ₽'; // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
-    newSumText = ${newSum} ₽;// замените склеивание, используемое в newSum Text на шаблонную строку
-
-    console.log(newSum);// Конец решения задания №1.3.
-
+  
+    // создайте переменную newSum и присвойте ей сумму oldSum и difference
+    let newSum = oldSum + difference;
+  
+    // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
+    let newSumText = `${newSum} ₽`;
+  
+    // Конец решения задания №1.3.
+  
     return newSumText;
-}
+  }
 
--- task_final/solution.js
+++ task_final/solution.js
@@ -32,22 +32,25 @@
 //      ]
 //    }
 // }
-
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+    let data = { goods: [], order: {} };
+  
     let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+  
+    for (let i = 0; i < countOfGoods; i += 1) {
+      let item = {
+        title: goods[i].title,
+        count: goods[i].count,
+      };
+      data.goods.push(item);
     }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
+  
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`;
+    data.order.sum = sum;
+  
+    data.client = `${name} ${phone}`;
+  
+    let jsonData = JSON.stringify({ data });
+  
     return jsonData;
-}
+  }
